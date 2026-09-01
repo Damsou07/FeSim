7cb20529bb8d9ead2f583da06e19ce9c4bd4aafd
Damien Angibaud <angibaud.pro@gmail.com>
2026-09-01 16:39:55 +0200

    ajout des données de cap

--- src/FeSim/database/entities/game_class.py
+++ src/FeSim/database/entities/game_class.py
@@ -16,10 +16,13 @@ class GameClass(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
 
+    # Niveau de la classe à que deux valeurs possible "pre promotion" ou post promotion"
+    level_class: Mapped[str] = mapped_column(String, nullable=False)
+
     game: Mapped[str] = mapped_column(String(100), nullable=False)
     class_name: Mapped[str] = mapped_column(String(100), nullable=False)
 
-    # Bonus obtenus lors de la promotion
+    # Bonus obtenus lors de la promotion, concerne uniquement les classes post promotion
     promotion_hp: Mapped[int] = mapped_column(Integer, default=0)
     promotion_str: Mapped[int] = mapped_column(Integer, default=0)
     promotion_mag: Mapped[int] = mapped_column(Integer, default=0)
@@ -28,6 +31,16 @@ class GameClass(Base):
     promotion_lck: Mapped[int] = mapped_column(Integer, default=0)
     promotion_def: Mapped[int] = mapped_column(Integer, default=0)
     promotion_res: Mapped[int] = mapped_column(Integer, default=0)
+
+    # Cap de classe
+    cap_hp: Mapped[int] = mapped_column(Integer, default=0)
+    cap_str: Mapped[int] = mapped_column(Integer, default=0)
+    cap_mag: Mapped[int] = mapped_column(Integer, default=0)
+    cap_skl: Mapped[int] = mapped_column(Integer, default=0)
+    cap_spd: Mapped[int] = mapped_column(Integer, default=0)
+    cap_lck: Mapped[int] = mapped_column(Integer, default=0)
+    cap_def: Mapped[int] = mapped_column(Integer, default=0)
+    cap_res: Mapped[int] = mapped_column(Integer, default=0)
 
     characters: Mapped[list[Character]] = relationship(
         back_populates="game_class"
